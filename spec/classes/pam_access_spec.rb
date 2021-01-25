f7a1e772ad9e57ed9142ee5185988063af116371
Frank Luo <luoy@miamioh.edu>
2021-01-25 14:28:33 -0500

    pdk convert

--- spec/classes/pam_access_spec.rb
+++ spec/classes/pam_access_spec.rb
@@ -2,27 +2,27 @@ require 'spec_helper'
 
 describe 'pam_access' do
   describe 'does stuff if os supported' do
-    let(:facts) { { osfamily => 'RedHat', operatingsystemrelease => '7.1' } }
-    let(:params) { { manage_pam => false } }
+    let(:facts) { { osfamily: 'RedHat', operatingsystemrelease: '7.1' } }
+    let(:params) { { manage_pam: false } }
 
-    it { is_epected.to compile.with_all_deps }
+    it { is_expected.to compile.with_all_deps }
 
     it do
-      is_epected.to contain_file('/etc/security/access.conf').with(
-        its('owner') { is_epected.to eq 'root' },
-        its('group') { is_epected.to eq 'root' },
-        its('mode') { is_epected.to eq '0644' },
+      is_expected.to contain_file('/etc/security/access.conf').with(
+        'owner' => 'root',
+        'group' => 'root',
+        'mode'  => '0644',
       )
     end
 
     describe 'execs authconfig-access' do
-      let(:params) { { manage_pam => true } }
+      let(:params) { { manage_pam: true } }
 
-      it { is_epected.to compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
 
       it do
-        is_epected.to contain_exec('authconfig-access').with(
-          command => '/usr/sbin/authconfig --enablelocauthorize --enablepamaccess --update',
+        is_expected.to contain_exec('authconfig-access').with(
+          'command' => '/usr/sbin/authconfig --enablelocauthorize --enablepamaccess --update',
         )
       end
     end
